88baf9ca7751f424f44492bec5775c299a3d97c6
Lauro Ribeiro <lauroribeirola@hotmail.com>
2021-05-09 01:09:51 -0300

    selecionando elementos no DOM

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let name = document.querySelector("#nome");
+let email = document.querySelector("#email");
+let cpf = document.querySelector("#cpf");
+let endereco = document.querySelectorAll(".endereco");
+let btn = document.querySelector("#submit");
+
+endereco.forEach((element) => console.log(element.innerText));
+// btn.addEventListener("click", cadastrar());
