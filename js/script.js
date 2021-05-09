f9b302a8ccf7adb353f0a5347bc5101c292f5111
Lauro Ribeiro <lauroribeirola@hotmail.com>
2021-05-09 12:02:54 -0300

    pegar dados dos inputs e criar obj pessoa, limpar inputs. Alteracao no html para retirar acao padrao do botao dentro do form

--- js/script.js
+++ js/script.js
@@ -2,7 +2,32 @@ let name = document.querySelector("#nome");
 let email = document.querySelector("#email");
 let cpf = document.querySelector("#cpf");
 let endereco = document.querySelectorAll(".endereco");
-let btn = document.querySelector("#submit");
+let btn = document.querySelector("#submitBtn");
+let pessoa;
+endereco_array = Array.from(endereco);
 
-endereco.forEach((element) => console.log(element.innerText));
-// btn.addEventListener("click", cadastrar());
+function cadastrar(){
+  let pessoa = {
+    nome: nome.value,
+    email: email.value,
+    cpf: cpf.value,
+    endereco: {
+      logradouro: endereco_array[0].value,
+      numero: endereco_array[1].value,
+      cidade: endereco_array[2].value,
+      bairro: endereco_array[3].value,
+      estado: endereco_array[4].value
+    }
+  } 
+  // limparInputs();
+  console.log(pessoa);
+}
+
+function limparInputs(){
+  nome.value = '';
+  email.value = '';
+  cpf.value = '';
+  endereco_array.forEach((e) => e.value = '');
+}
+
+btn.addEventListener("click", cadastrar);
